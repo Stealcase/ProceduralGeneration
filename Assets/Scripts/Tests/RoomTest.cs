d40940d452325a61ff710252334d587faa2571d7
Benjamin <stealcase@hotmail.com>
2021-06-29 01:38:20 +0200

    Setting up Tests for Generators

--- Assets/Scripts/Tests/RoomTest.cs
+++ Assets/Scripts/Tests/RoomTest.cs
@@ -11,11 +11,42 @@ namespace Tests
     {
         // A Test behaves as an ordinary method
         [Test]
-        public void TestRoomCreation()
+        public void TestRoomBottomLeft()
         {
-            var room = new Room(new Vector2Int(0, 0), new Vector2Int(10, 10), 5);
-
+            var origin = new Vector2Int(0, 0);
+            var size = new Vector2Int(10, 10);
+            RectInt rect = new RectInt(origin, size);
+            var room = new Room(origin, size, 5);
+            Assert.IsTrue(rect.Contains(room.BottomLeft));
         }
+        [Test]
+        public void TestRoomTopRight()
+        {
+            var origin = new Vector2Int(0, 0);
+            var size = new Vector2Int(10, 10);
+            RectInt rect = new RectInt(origin, size);
+            var room = new Room(origin, size, 5);
+            Assert.IsTrue(rect.Contains(room.TopRight));
+        }
+        [Test]
+        public void TestRoomWidth()
+        {
+            var origin = new Vector2Int(0, 0);
+            var size = new Vector2Int(10, 10);
+            RectInt rect = new RectInt(origin, size);
+            var room = new Room(origin, size, 5);
+            Assert.IsTrue(room.Width >= 5);
+        }
+        [Test]
+        public void TestRoomHeight()
+        {
+            var origin = new Vector2Int(0, 0);
+            var size = new Vector2Int(10, 10);
+            RectInt rect = new RectInt(origin, size);
+            var room = new Room(origin, size, 5);
+            Assert.IsTrue(room.Height >= 5);
+        }
+
 
     }
 }
--- Assets/Scripts/Tests/TestNumberGenerator.cs
+++ Assets/Scripts/Tests/TestNumberGenerator.cs
@@ -18,6 +18,44 @@ namespace Tests
             Assert.Greater(num,6);
             Assert.Less(num,11);
         }
+        [Test]
+        public void TestRectWithinBoundsWidth()
+        {
+            var origin = new Vector2Int(0, 0);
+            var size = new Vector2Int(10, 10);
+            RectInt rect = VectorHelper.RectWithinBounds(origin, size, 2);
+
+            Assert.IsTrue(rect.width >= 2);
+        }
+        [Test]
+        public void TestRectWithinBoundsHeight()
+        {
+            var origin = new Vector2Int(0, 0);
+            var size = new Vector2Int(10, 10);
+            RectInt rect = VectorHelper.RectWithinBounds(origin, size, 2);
+
+            Assert.IsTrue(rect.height >= 2);
+        }
+        [Test]
+        public void TestRectWithinBoundsWithinBoundsLeft()
+        {
+            var origin = new Vector2Int(0, 0);
+            var size = new Vector2Int(10, 10);
+            var container = new RectInt(origin, size);
+            RectInt rect = VectorHelper.RectWithinBounds(origin, size, 2);
+
+            Assert.IsTrue(container.Contains(rect.min));
+        }
+        [Test]
+        public void TestRectWithinBoundsWithinBoundsRight()
+        {
+            var origin = new Vector2Int(0, 0);
+            var size = new Vector2Int(10, 10);
+            var container = new RectInt(origin, size);
+            RectInt rect = VectorHelper.RectWithinBounds(origin, size, 2);
+
+            Assert.IsTrue(container.Contains(rect.max));
+        }
 
         // // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
         // // `yield return null;` to skip a frame.
