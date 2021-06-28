692b31162c4f7744b6c7355112148b654c5cb9ac
Benjamin <stealcase@hotmail.com>
2021-06-29 01:25:26 +0200

    Writing Initial Tests

--- Assets/Scripts/Tests/NodeTest.cs
+++ Assets/Scripts/Tests/NodeTest.cs
@@ -11,11 +11,64 @@ namespace Tests
     {
         // A Test behaves as an ordinary method
         [Test]
-        public void TestNode()
+        public void HorizontalSplitLocationSymmetry()
         {
-            var room = new Room(new Vector2Int(0, 0), new Vector2Int(10, 10), 5);
-
+            var rand = new System.Random();
+            var rootNode = new Stealcase.Generators.Procedural.BSP.Node(new RectInt(new Vector2Int(0, 0), new Vector2Int(10, 10)),0, null, 0, 3, 5, rand);
+            var tup = rootNode.CreateHorizontalSplit();
+            var left = tup.Item1;
+            var right = tup.Item2;
+            Assert.AreEqual(left.yMax, right.yMin);
         }
-
+        [Test]
+        public void HorizontalSplitTopMaxInsideRect()
+        {
+            var rand = new System.Random();
+            var rootNode = new Stealcase.Generators.Procedural.BSP.Node(new RectInt(new Vector2Int(0, 0), new Vector2Int(10, 10)),0, null, 0, 3, 5, rand);
+            var tup = rootNode.CreateHorizontalSplit();
+            var left = tup.Item1;
+            var right = tup.Item2;
+            Assert.AreEqual(rootNode.Rect.max, right.max);
+        }
+        [Test]
+        public void HorizontalSplitTopMinInsideRect()
+        {
+            var rand = new System.Random();
+            var rootNode = new Stealcase.Generators.Procedural.BSP.Node(new RectInt(new Vector2Int(0, 0), new Vector2Int(10, 10)),0, null, 0, 3, 5, rand);
+            var tup = rootNode.CreateHorizontalSplit();
+            var left = tup.Item1;
+            var right = tup.Item2;
+            Assert.IsTrue(rootNode.Rect.Contains(right.min));
+        }
+        [Test]
+        public void HorizontalSplitLeftWallCorrect()
+        {
+            var rand = new System.Random();
+            var rootNode = new Stealcase.Generators.Procedural.BSP.Node(new RectInt(new Vector2Int(0, 0), new Vector2Int(10, 10)),0, null, 0, 3, 5, rand);
+            var tup = rootNode.CreateHorizontalSplit();
+            var left = tup.Item1;
+            var right = tup.Item2;
+            Assert.AreEqual(left.xMin, rootNode.Rect.xMin);
+        }
+        [Test]
+        public void HorizontalSplitBottomMinInsideNode()
+        {
+            var rand = new System.Random();
+            var rootNode = new Stealcase.Generators.Procedural.BSP.Node(new RectInt(new Vector2Int(0, 0), new Vector2Int(10, 10)),0, null, 0, 3, 5, rand);
+            var tup = rootNode.CreateHorizontalSplit();
+            var left = tup.Item1;
+            var right = tup.Item2;
+            Assert.IsTrue(rootNode.Rect.Contains(left.min));
+        }
+        [Test]
+        public void HorizontalSplitBottomXMaxInsideRect()
+        {
+            var rand = new System.Random();
+            var rootNode = new Stealcase.Generators.Procedural.BSP.Node(new RectInt(new Vector2Int(0, 0), new Vector2Int(10, 10)),0, null, 0, 3, 5, rand);
+            var tup = rootNode.CreateHorizontalSplit();
+            var left = tup.Item1;
+            var right = tup.Item2;
+            Assert.AreEqual(rootNode.Rect.xMax, left.xMax);
+        }
     }
 }
