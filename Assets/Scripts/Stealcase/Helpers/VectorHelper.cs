c8569bb5ac131d1746adab04ff9a979b899c0683
Benjamin <stealcase@hotmail.com>
2021-07-04 15:34:31 +0200

    Using Seed to generate Map

--- Assets/Scripts/Stealcase/Helpers/VectorHelper.cs
+++ Assets/Scripts/Stealcase/Helpers/VectorHelper.cs
@@ -8,7 +8,7 @@ namespace Stealcase.Helpers
 
 public static class VectorHelper 
 {
-    public static System.Random random = new System.Random();
+
     /// <summary>
     /// Returns a rectangle that fits anywhere within the defined square.
     /// Rectangle has a minimum width and height of minimunSize
@@ -17,49 +17,54 @@ public static class VectorHelper
     /// <param name="TopRight"></param>
     /// <param name="minimumSize"></param>
     /// <returns></returns>
-    public static Tuple<Vector2Int, Vector2Int> RectWithinRect(Vector2Int BottomLeft, Vector2Int TopRight, int minimumSize) 
+    public static Tuple<Vector2Int, Vector2Int> RectWithinRect(Vector2Int BottomLeft, Vector2Int TopRight, int minimumSize, System.Random rand = null) 
     {
-
+        rand = rand ?? new System.Random();
         //The highest number between these two
         //Pick a spot
         //Then Try to find a spot that
         var leftX_MaxCoord = Mathf.Max(TopRight.x - minimumSize, BottomLeft.x);
-        int leftXCoord = random.Next(BottomLeft.x, leftX_MaxCoord);
+        int leftXCoord = rand.Next(BottomLeft.x, leftX_MaxCoord);
         //Lowest number between these two
         var rightXMinCoord = Mathf.Min(leftXCoord + minimumSize, TopRight.x);
-        int rightXCoord = random.Next(rightXMinCoord, TopRight.x);
+        int rightXCoord = rand.Next(rightXMinCoord, TopRight.x);
 
 
 
         var leftY_MaxCoord = Mathf.Max(TopRight.y - minimumSize, BottomLeft.y);
-        var leftYCoord = random.Next(BottomLeft.y, leftY_MaxCoord);
+        var leftYCoord = rand.Next(BottomLeft.y, leftY_MaxCoord);
         
         var rightYMinCoord = Mathf.Min(leftYCoord + minimumSize, TopRight.y);
-        var rightYCoord = random.Next(rightYMinCoord, TopRight.y);
+        var rightYCoord = rand.Next(rightYMinCoord, TopRight.y);
 
         var _bottomLeft = new Vector2Int(leftXCoord,leftYCoord);
         var _topRight = new Vector2Int(rightXCoord,rightYCoord);
 
         return new Tuple<Vector2Int, Vector2Int>(_bottomLeft, _topRight);
     }
-    public static RectInt RectWithinBounds(Vector2Int BottomLeft, Vector2Int TopRight, int minimumSize) 
+    public static RectInt RectWithinBounds(Vector2Int BottomLeft, Vector2Int TopRight, int minimumSize, int margin = 0, System.Random rand = null) 
     {
-
+        rand = rand ?? new System.Random();
         //The highest number between these two
         //Pick a spot
         //Then Try to find a spot that
-        var leftX_MaxCoord = Mathf.Max(TopRight.x - minimumSize, BottomLeft.x);
-        int leftXCoord = random.Next(BottomLeft.x, leftX_MaxCoord);
+        var leftX_MaxCoord = Mathf.Max(TopRight.x - (minimumSize + margin), BottomLeft.x + margin);
+        int leftXCoord = rand.Next(BottomLeft.x + margin, leftX_MaxCoord);
         //Lowest number between these two
-        var rightXMinCoord = Mathf.Min(leftXCoord + minimumSize, TopRight.x);
-        int rightXCoord = random.Next(rightXMinCoord, TopRight.x);
+
+        //Define smallest possible x Coord
+        var rightXMinCoord = Mathf.Min(leftXCoord + minimumSize, TopRight.x - margin);
+        //Randomly Define Size:
+        int rightXCoord = rand.Next(rightXMinCoord, TopRight.x - margin);
         var width = rightXCoord - leftXCoord;
 
-        var leftY_MaxCoord = Mathf.Max(TopRight.y - minimumSize, BottomLeft.y);
-        var leftYCoord = random.Next(BottomLeft.y, leftY_MaxCoord);
+        //Define biggest possible x Coord
+        var leftY_MaxCoord = Mathf.Max(TopRight.y - (minimumSize + margin), BottomLeft.y + margin);
+        var leftYCoord = rand.Next(BottomLeft.y, leftY_MaxCoord - margin);
         
-        var rightYMinCoord = Mathf.Min(leftYCoord + minimumSize, TopRight.y);
-        var rightYCoord = random.Next(rightYMinCoord, TopRight.y);
+        //Randomly Define Size:
+        var rightYMinCoord = Mathf.Min(leftYCoord + minimumSize, TopRight.y - margin);
+        var rightYCoord = rand.Next(rightYMinCoord, TopRight.y - margin);
         var height = rightYCoord - leftYCoord;
 
         return new RectInt(leftXCoord, leftYCoord, width, height);
@@ -81,7 +86,7 @@ public static class VectorHelper
 /// <param name="max"></param>
 /// <param name="margin"></param>
 /// <returns></returns>
-    public static int NumberBetweenNumbers(int min, int max, int margin)
+    public static int NumberBetweenNumbers(int min, int max, int margin, System.Random rand = null)
     {
         if(max - min < margin)
         {
@@ -90,8 +95,9 @@ public static class VectorHelper
         }
         var minimum = min + margin;
         var maximum = max - margin;
+        rand = rand ?? new System.Random();
         maximum = Mathf.Max(minimum, maximum);
-        var randomPoint = random.Next(minimum, maximum);
+        var randomPoint = rand.Next(minimum, maximum);
         return randomPoint;
     }
 
